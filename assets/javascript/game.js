31d11398e3e9a0c6f14015f7297bfd746654f7ed
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-08-29 19:39:31 -0700

    changed id guess to uiGuess ; rain prettier format

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,55 +5,61 @@ var tries = 10;
 var container = document.getElementById("tracker");
 var guess = [];
 var target = String.fromCharCode(97 + Math.floor(Math.random() * 26));
-console.log(target)
+console.log(target);
 
 function resetGame() {
-    target = String.fromCharCode(97 + Math.floor(Math.random() * 26));
-    console.log(target)
-    tries = 10;
-    guess = [];
-    document.querySelector("#attempts").innerHTML = tries;
-    document.querySelector("#guess").innerHTML = guess;
+  target = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  console.log(target);
+  tries = 10;
+  guess = [];
+  document.querySelector("#attempts").innerHTML = tries;
+  document.querySelector("#guess").innerHTML = guess;
 }
 
 function logScore(endGame) {
-    var paragraph = document.createElement("P");
-    paragraph.textContent = `Game#: ${win + lose} | ${endGame} | Goal: ${target} | Guesses(${guess.length}): ${guess.join(" ")}`;
-    if (endGame === "Win") {
-        paragraph.setAttribute("class", "bg-primary text-light p-1");
-    } else {
-        paragraph.setAttribute("class", "bg-danger text-light p-1");
-    }
+  var paragraph = document.createElement("P");
+  paragraph.textContent = `Game#: ${win + lose} |
+   ${endGame} | Goal: ${target} |
+    Guesses(${guess.length}): ${guess.join(" ")}`;
+  if (endGame === "Win") {
+    paragraph.setAttribute("class", "bg-primary text-light p-1");
+  } else {
+    paragraph.setAttribute("class", "bg-danger text-light p-1");
+  }
 
-    container.appendChild(paragraph);
+  container.appendChild(paragraph);
 }
-document.onkeypress = function (event) {
-    // console.log(event.key);
-    if (!((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122))) {
-        console.log("invalid letter");
-        return;
+document.onkeypress = function(event) {
+  // console.log(event.key);
+  if (
+    !(
+      (event.keyCode >= 65 && event.keyCode <= 90) ||
+      (event.keyCode >= 97 && event.keyCode <= 122)
+    )
+  ) {
+    console.log("invalid letter");
+    return;
+  }
+  if (event.key === target) {
+    guess.push(event.key);
+    // console.log("you win");
+    win++;
+    document.querySelector("#winScore").innerHTML = win;
+    logScore("Win");
+    resetGame();
+  } else if (!guess.includes(event.key)) {
+    tries--;
+    guess.push(event.key);
+    // console.log("wrong", guess);
+    document.querySelector("#attempts").innerHTML = tries;
+    document.querySelector("#uiGuess").innerHTML = guess.join(" ");
+    if (tries === 0) {
+      lose++;
+      document.querySelector("#loseScore").innerHTML = lose;
+      logScore("Lose");
+      resetGame();
     }
-    if (event.key === target) {
-        guess.push(event.key);
-        // console.log("you win");
-        win++;
-        document.querySelector("#winScore").innerHTML = win;
-        logScore("Win");
-        resetGame();
-    } else if (!guess.includes(event.key)) {
-        tries--;
-        guess.push(event.key);
-        // console.log("wrong", guess);
-        document.querySelector("#attempts").innerHTML = tries;
-        document.querySelector("#guess").innerHTML = guess.join(" ");
-        if (tries === 0) {
-            lose++;
-            document.querySelector("#loseScore").innerHTML = lose;
-            logScore("Lose");
-            resetGame();
-        }
-    } else {
-        alert("Already Guessed that letter");
-    }
-
-}
+  } else {
+    alert("Already Guessed that letter");
+  }
+};
